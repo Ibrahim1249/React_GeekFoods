e891f56fa085eb04c5e8498ba05b9db2cfc16d45
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-05-27 12:39:24 +0530

    completed assignment on geekfoods

---
+++ components/Section.jsx
@@ -0,0 +1,20 @@
+
+function Section(){
+    return(
+        <>
+         <div className="section-container">
+            <div className="left-section">
+                <img src="/src/assets/section.jpg" alt="" />
+            </div>
+            <div className="right-section">
+                <div className="right-contain">
+                    <p className="para-head">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</p>
+                    <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.</p>
+                    <button className="btn-sec">Get in Touch</button>
+                </div>
+            </div>
+         </div>
+        </>
+    )
+}
+export default Section;
